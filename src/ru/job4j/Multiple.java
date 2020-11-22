548dc8cb57321915e041ddaf13cc835d7dcc847f
Mn3ment <muviment@gmail.com>
2020-11-23 00:13:26 +0300

    3.3. Разбор первой программы. [#415687]

---
+++ src/ru/job4j/Multiple.java
@@ -0,0 +1,12 @@
+package ru.job4j;
+
+public class Multiple {
+
+    public static void main(String[] args) {
+        int j = 1;
+        for (int i = 2; i <= 9; i++) {
+                System.out.print(j + "x" + i + "=" + (i * j) + "\n");
+            }
+
+    }
+}
