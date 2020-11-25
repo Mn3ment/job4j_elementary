4b7cb4163aa46e88f45772cb7f75590c8f852083
Mn3ment <muviment@gmail.com>
2020-11-25 22:51:08 +0300

    9.1. String [#415677]

--- src/ru/job4j/Echo.java
+++ src/ru/job4j/Echo.java
@@ -1,5 +1,10 @@
 package ru.job4j;
 
 public class Echo {
-
+    public static void main(String[] args) {
+        short s = 1500;
+        char c = (char) s;
+        double d = c;
+        System.out.println("Результат вычисления: " + d);
+    }
 }
--- src/ru/job4j/condition/Greeting.java
+++ src/ru/job4j/condition/Greeting.java
@@ -5,8 +5,10 @@ public class Greeting {
     public static void main(String[] args) {
 
         String idea = "I like Java!";
+        System.out.println(idea);
+        idea = idea + " But I'm newbie. ";
         int year = 2020;
-        idea = idea + " But I'm newbie. " + year;
+        idea = idea + year;
 
         System.out.println(idea);
     }
