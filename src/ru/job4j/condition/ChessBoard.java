f80e4889af62b36eee63cdd09ab3c8d904e153ff
Mn3ment <muviment@gmail.com>
2021-01-11 16:26:51 +0300

    4.5. Отладка программы в IDEA [#415613]

--- src/ru/job4j/condition/ChessBoard.java
+++ src/ru/job4j/condition/ChessBoard.java
@@ -3,7 +3,7 @@ package ru.job4j.condition;
 public class ChessBoard {
     public static int way(int x1, int y1, int x2, int y2) {
         int rsl = 0;
-        if ((x2 - x1) == (y2 - y1) || (x2 + x1) == (y2 + y1)) {
+        if ((x2 - x1) == (y2 - y1)) {
             rsl = Math.abs(x2 - x1);
             return rsl;
         }
--- src/ru/job4j/condition/MultiMax.java
+++ src/ru/job4j/condition/MultiMax.java
@@ -2,16 +2,14 @@ package ru.job4j.condition;
 
 public class MultiMax {
     public static int max(int first, int second, int third) {
-        int rsl = first;
-        if ((first >= second) && (first >= third)) {
+        int rsl = third;
+        if ((first < second) || (first < third)) {
+            if ((second >= first) && (second >= third)) {
+                    rsl = second;
+                }
+        } else {
             rsl = first;
-        }
-        else if ((second >= first) && (second >= third)) {
-                rsl = second;
             }
-        else {
-            rsl = third;
-        }
         return rsl;
     }
 }
