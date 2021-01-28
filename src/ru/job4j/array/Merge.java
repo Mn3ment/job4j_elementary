92a3ac8901bb3dfb6f67d744f2bbafbea1adb511
Mn3ment <muviment@gmail.com>
2021-01-28 22:47:28 +0300

    merge fix

--- src/ru/job4j/array/Merge.java
+++ src/ru/job4j/array/Merge.java
@@ -3,22 +3,15 @@ package ru.job4j.array;
 public class Merge {
     public static int[] merge(int[] left, int[] right) {
         int[] rsl = new int[left.length + right.length];
-        int index = 0;
-        for (int i = 0; i < left.length; i++) {
-            rsl[i] = left[i];
-            index++;
-        }
-        for (int j = 0; j < right.length; j++) {
-            rsl[index] = right[j];
-            index++;
-        }
-        for (int k = 0; k < rsl.length; k++) {
-            for (int l = rsl.length - 1; l > k; l--) {
-                if (rsl[l - 1] > rsl[l]) {
-                    int tmp = rsl[l - 1];
-                    rsl[l - 1] = rsl[l];
-                    rsl[l] = tmp;
-                }
+        int i = 0;
+        int j = 0;
+        for (int index = 0; index < rsl.length; index++) {
+            if (i < left.length && j < right.length) {
+                rsl[index] = left[i] > right[j] ? right[j++] : left[i++];
+            } else if (j < right.length) {
+                rsl[index] = right[j++];
+            } else {
+                rsl[index] = left[i++];
             }
         }
         return rsl;
